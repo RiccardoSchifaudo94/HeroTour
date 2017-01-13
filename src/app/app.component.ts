c0885927378414d770c7da11de1f4e3abcb7730f
Riccardo <mr.emps94@gmail.com>
2017-01-13 23:14:21 +0100

    creazione dei componenti angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {Hero} from './hero';
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  welcome = 'Welcome in the Hero Tour!';
-  hero:	Hero 	=	{ 
+
+export class AppComponent implements OnInit {
+    welcome = 'Welcome in the Hero Tour!';
+    hero:	Hero 	=	{ 
 
   						id:1,
   						name:'Windstorm'
   					}
+
+    heroes = HEROES;
+    selectedHero:Hero;
+
+    onSelect(hero:Hero):void{
+    	this.selectedHero = hero;
+    }
+
+    ngOnInit(){
+      alert("lancio app!");
+    }
 }
-export class Hero{
-	id:number;
-	name:string;
-}
+
+
+
+
+
