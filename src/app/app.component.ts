7852a7b548c59d1876ec1628d825457883b9dc53
Riccardo <mr.emps94@gmail.com>
2017-01-14 23:28:03 +0100

    creazione di mock heroes con dependency injection e regstrazione provide nell'appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,22 @@
 import { Component,OnInit } from '@angular/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HEROES } from './mock-heroes';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
 
 export class AppComponent implements OnInit {
+    constructor(private heroService:HeroService){
+
+    }
+
+
     welcome = 'Welcome in the Hero Tour!';
     hero:	Hero 	=	{ 
 
